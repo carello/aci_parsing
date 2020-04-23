139f8d4111ed81c17a0d7b06d0541e8c35a62663
carello <cp2carello@gmail.com>
2020-04-23 15:28:46 -0500

    added get setter example - nice
    	new file:   ray_hettinger/set_get4.py

---
+++ ray_hettinger/set_get4.py
@@ -0,0 +1,29 @@
+class House(object):
+    def __init__(self, price):
+        self._price = price
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price > 0 and isinstance(new_price, float):
+            self._price = new_price
+        else:
+            print("Please enter a valid price")
+
+    @price.deleter
+    def price(self):
+        print("boo")
+        del self._price
+
+
+house = House(222.34)
+print(house.price)
+house.price = 150.01
+print(house.price)
+del house.price
+
+
+
